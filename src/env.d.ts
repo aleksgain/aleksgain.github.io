2f7be987e7b97a4752e9548f76edd8355ecc7fb8
Alex Gain <alg@revolgy.com>
2025-01-04 17:58:31 +0100

    add certs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,6 @@
+/// <reference path="../.astro/types.d.ts" />
 interface ImportMetaEnv {
   readonly PUBLIC_GITHUB_TOKEN: string;
-  readonly LINKEDIN_CLIENT_ID: string;
-  readonly LINKEDIN_CLIENT_SECRET: string;
 }
 
 interface ImportMeta {
